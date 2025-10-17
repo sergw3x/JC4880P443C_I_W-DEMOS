f8043c8be951d5adbc23836daa07c7e86799eaeb
Shklyar Sergey <sergw3x@gmail.com>
2025-10-17 13:33:48 +0300

    py

--- my_serial_script.py
+++ my_serial_script.py
@@ -4,27 +4,35 @@ import time
 serial_port = '/dev/tty.usbmodem1101'
 baud_rate = 9600
 # DATA_TO_SEND = '{"type": "qr", "data": "https://moskvarium.ru"}\n'
-DATA_TO_SEND = '{"type": "qr", "data": "https://example.com"}\n'
+# DATA_TO_SEND = '{"type": "qr", "data": "https://example.com"}\n'
 
+data = [
+    '{"type": "qr", "data": "https://example.com"}\n',
+    '{"type": "qr", "data": "https://moskvarium.ru"}\n',
+    '{"type": "qr", "data": "https://google.com"}\n',
+    '{"type": "qr", "data": "https://yahoo.com"}\n',
+]
 
 try:
     # Инициализация с таймаутом в 1 секунду
     ser = serial.Serial(serial_port, baud_rate, timeout=1)
     print(f"Opened serial port {ser.name}")
     time.sleep(2)  # Пауза для сброса устройства
+    for row in data:
 
-    # Отправляем команду
-    message = DATA_TO_SEND.encode('utf-8')
-    ser.write(message)
-    print(f"Отправлены данные: {DATA_TO_SEND.strip()}")
+        # Отправляем команду
+        message = row.encode('utf-8')
+        ser.write(message)
+        print(f"Отправлены данные: {row.strip()}")
 
-# Читаем ответ от устройства
-    response = ser.readline()
-    if response:
-        # Декодируем и выводим ответ
-        print(f"Received: {response.decode('utf-8').strip()}")
-    else:
-        print("Received: No response within timeout")
+        # Читаем ответ от устройства
+        response = ser.readline()
+        if response:
+            # Декодируем и выводим ответ
+            print(f"Received: {response.decode('utf-8').strip()}")
+        else:
+            print("Received: No response within timeout")
+        time.sleep(1)
 
     ser.close()
 
